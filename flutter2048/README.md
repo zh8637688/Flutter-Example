39dfba7a8fc00e19b806272a2f2c33c7b083d457
HaoZhou <joe.h0430@gmail.com>
2018-06-22 18:07:35 +0800

    init project

---
+++ flutter2048/README.md
@@ -0,0 +1,8 @@
+# flutter2048
+
+flutter 2048
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ flutter2048/android/app/src/main/java/cz/flutter2048/MainActivity.java
@@ -0,0 +1,14 @@
+package cz.flutter2048;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
