3dddd90b02a0989205da12cfd398c90cc9e1aca1
HaoZhou <joe.h0430@gmail.com>
2018-07-20 11:42:43 +0800

    高版本中部分接口无法使用

--- zhihu_daily/lib/constants/urls.dart
+++ zhihu_daily/lib/constants/urls.dart
@@ -1,5 +1,6 @@
 class Urls {
-  static const String HOST = 'https://news-at.zhihu.com/api/7';
+  static const String HOST = 'https://news-at.zhihu.com/api/4';
 
   static const String THEME_LIST = HOST + '/themes';
+  static const String NEWS_LAST = HOST + '/news/latest';
 }
